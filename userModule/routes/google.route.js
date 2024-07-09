8d1915eb286b2621e3eddd94897892728c85e721
Samuel2601 <saamare99@gmail.com>
2024-07-09 11:30:39 -0500

    correcion de prefijos2

--- userModule/routes/google.route.js
+++ userModule/routes/google.route.js
@@ -19,7 +19,7 @@ const router = express.Router();
 
 /**
  * @swagger
- * /new/auth/google:
+ * /auth/google:
  *   get:
  *     summary: Iniciar autenticaci�n de Google
  *     tags: [GoogleAuth]
@@ -30,7 +30,7 @@ const router = express.Router();
 
 /**
  * @swagger
- * /new/auth/google/callback/:
+ * /auth/google/callback/:
  *   get:
  *     summary: Callback de autenticaci�n de Google
  *     tags: [GoogleAuth]
@@ -55,7 +55,7 @@ const router = express.Router();
  */
 /**
  * @swagger
- * /new/logout:
+ * /logout:
  *   get:
  *     summary: Cerrar Sesi�n
  *     tags: [GoogleAuth]
