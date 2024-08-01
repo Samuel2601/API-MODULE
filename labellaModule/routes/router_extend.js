be43cef61649067c53734625e8e9d6d165bb3f3c
Samuel2601 <saamare99@gmail.com>
2024-08-01 12:29:08 -0500

    rutas para los recolectores3

--- labellaModule/routes/router_extend.js
+++ labellaModule/routes/router_extend.js
@@ -3,11 +3,10 @@
 import express from 'express';
 import { getCiudadano } from '../../apiservices/sinardap.js';
 import { updateRoutesOnDemand } from '../controllers/Recolectores_api.js';
-import { auth, permissUser, validateAuth } from '../../middlewares/validationResultExpress.js';
+import { auth, permissUser } from '../../middlewares/validationResultExpress.js';
 const router_extend = express.Router();
 router_extend.get('/getciudadano/:id', async (req, res) => {
     var id = req.params['id'];
-    console.log(id);
     try {
         const ciudadano = await getCiudadano(id);
         res.status(200).json(ciudadano);
@@ -25,6 +24,6 @@ router_extend.put(
       res.status(response.status).json(response);
     }
   );
-validateAuth
+
 
 export default router_extend;
